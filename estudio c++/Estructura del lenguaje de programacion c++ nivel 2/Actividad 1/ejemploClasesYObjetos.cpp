ea9478e8d7b670d1ba7ea0c6d7af762b83d6416a
ryuksan <clinicasystemlab@gmail.com>
2023-04-05 01:41:47 -0500

    se agrega descripcion de la funcionalidad

--- estudio c++/Estructura del lenguaje de programacion c++ nivel 2/Actividad 1/ejemploClasesYObjetos.cpp
+++ estudio c++/Estructura del lenguaje de programacion c++ nivel 2/Actividad 1/ejemploClasesYObjetos.cpp
@@ -1,7 +1,26 @@
+/*En el siguiente ejemplo se observa la creación de una clase con sus atributos y métodos en
+C++; así como también la instanciación de la misma a través de la creación de dos objetos
+a partir de ella. Se recomienda digitar el código en el Dev-C++ para continuar con la
+práctica del lenguaje.
+A continuación, se explican los apartes del código donde se realiza la creación de la clase,
+se instancia y se ejecutan los métodos.*/
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-
+/*Creación de la clase
+En la línea 5 se da el nombre a la clase y se abre llave para incluir sus atributos y
+métodos.
+En la línea 6 se indica que los elementos declarados a continuación son privados y éstos
+van a ser los atributos de la clase.
+En las líneas 7,8 y 9 se declaran los atributos de la clase que son: cédula, nombre y
+teléfono de tipo string.
+En la línea 10 se indica que los elementos declarados a continuación son públicos y estos
+van a ser los métodos de la clase.
+En la línea 11 se indica el método constructor de la clase que sirve para inicializar las
+variables, debe tener el mismo nombre de la clase y entre paréntesis los tipos de los
+datos a inicializar.
+En las líneas 12 y 13 se indican los métodos comprar y alquilar de la clase.
+En la línea 14 se cierra la llave y se pone el punto y coma (;).*/
 class Cliente {
   private: 
     string cedula;
@@ -12,13 +31,30 @@ class Cliente {
     void comprar();
     void alquilar();
 };
-
+/*Inicialización del constructor
+En la línea 16 se inicializa el constructor para lo cual debe indicarse el nombre (Cliente) y
+de qué tipo es (Cliente) y a continuación entre paréntesis los datos que recibe con su
+respectivo tipo, usualmente se utiliza un nombre diferente al que se dio a las variables.
+En las líneas 17, 18 y 19 se indica que la cédula, el nombre y el teléfono deben ser iguales
+a las nuevas variables que serán ingresadas cuando se cree el objeto.*/
 Cliente::Cliente(string laCedula, string elNombre, string elTelefono){
   cedula = laCedula;
   nombre = elNombre;
   telefono = elTelefono;
 }
-
+/*Creación de métodos
+Entre las líneas 22 y 24 se crea el método comprar, lo primero que debe hacerse es
+indicar de que tipo es (void), de qué clase (Cliente), posteriormente el nombre del
+método (comprar) y entre las llaves se incluyen las instrucciones que determinan qué es
+exactamente lo que el método debe realizar; para el ejemplo se muestra un texto en
+pantalla que incluye una de las variables.
+Entre las líneas 26 y 29 se realiza lo mismo que en el paso anterior, para el método
+alquilar, que también es de tipo void y de la clase Cliente. Dentro de las instrucciones se
+indica que imprima dos líneas de texto donde además se observan algunas de las
+variables.
+Es importante en la creación de los métodos indicar a qué clase pertenecen, ya que los
+programas contienen múltiples clases y en varias de ellas puede haber métodos con el
+mismo nombre.*/
 void Cliente::comprar(){
   cout<< "El cliente " <<nombre<< " ha comprado un videojuego"<<endl;
 }
@@ -26,7 +62,13 @@ void Cliente::alquilar(){
   cout<< "Videojuego alquilado a " << nombre << " con documento " <<cedula<<endl;
   cout<< "En caso de demoras en la entrega llamar al " <<telefono<<endl;
 }
-
+/*Creación de objetos
+En la línea 32 se crea el objeto llamado cliente1 de la clase Cliente inicializado con los
+valores cédula 10278548, nombre Luis Hurtado y teléfono 2156478524.
+En la línea 33 se crea el objeto llamado cliente2 de la clase Cliente inicializado con los
+valores cédula 24342548, nombre Andrea Moreno y teléfono 2627788.
+En las líneas 35 y 36 se indica a los objetos cliente1 y cliente2 que ejecuten los métodos
+alquilar y comprar, respectivamente.*/
 int main(){
   Cliente cliente1 = Cliente("10278548", "Luis Hurtado", "3156478524");
   Cliente cliente2 = Cliente("24342548", "Andrea Moreno", "2627788");
--- estudio c++/Estructura del lenguaje de programacion c++ nivel 2/Actividad 1/herencia.cpp
+++ estudio c++/Estructura del lenguaje de programacion c++ nivel 2/Actividad 1/herencia.cpp
@@ -1,6 +1,12 @@
+/*En el siguiente ejemplo se observa la aplicación de la herencia en C++ a través de la creación de un programa con una superclase llamada Persona,
+que tiene una clase hija llamada Empleado. Se debe digitar el código en el Dev-C++ para continuar con la práctica del lenguaje.
+A continuación, se explican los apartes del código donde se realiza la creación de la clase padre (Persona) y el proceso de herencia en la clase hija (Empleado).*/
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+/*Clase padre "Persona"
+Entre las líneas 5 y 12 se crea la clase Persona con sus respectivos atributos y métodos,
+dicha creación se realiza del mismo modo en que fue explicado para la figura Inicialización del constructor en el ejemplo 1*/
 class Persona {
   private:
     string nombre;
@@ -9,7 +15,13 @@ class Persona {
     Persona(string,int);
     void verPersona();
 };
-
+/*Clase hija "Empleado"
+En la línea 14 se define la clase Empleado y para indicar que es una clase hija; es decir, que hereda de la clase Persona, se escriben los dos puntos,
+la palabra public y el nombre de la clase padre.  En este caso el public significa que la clase Empleado puede manipular todos los elementos públicos de
+la clase Persona. En las líneas 16 y 17 se declaran los atributos propios de la clase Empleado. En la línea 19 se declara el constructor de la clase Empleado
+donde se inicializan tanto los atributos heredados (nombre y edad) como los atributos propios (cargo y salario). Se debe conservar el orden de ocurrencia de
+dichos atributos; es decir, incluir primero los de la clase padre y luego los de la clase hija. En la línea 20 se incluye un método llamado verEmpleado()
+propio de la clase Empleado; por lo tanto, dicha clase tiene un método propio llamado verEmpleado() y un método heredado llamado verPersona();*/
 class Empleado : public Persona{
   private: 
     string cargo;
@@ -18,17 +30,28 @@ class Empleado : public Persona{
     Empleado(string,int,string,float);
     void verEmpleado();
 };
-
+/*Inicialización del constructor de la clase "Persona"
+Entre las líneas 23 y 26 se inicializa el constructor de la clase Persona; lo cual  se realiza del mismo modo en que fue explicado para la figura
+Inicialización del constructor en el ejemplo 1.*/
 Persona::Persona(string elNombre,int laEdad){
   nombre = elNombre;
   edad = laEdad;
 }
-
+/*Inicialización del constructor de la clase "Empleado"
+En la línea 28 se inicializa el constructor, para lo cual debe indicarse el nombre (Empleado) y de qué tipo es (Empleado) y a continuación entre paréntesis
+los datos que recibe con su respectivo tipo. Teniendo en cuenta que esta es la clase hija, deben incluirse tanto los atributos heredados como los propios;
+sin embargo, los datos de los atributos heredados ya fueron indicados, razón por la cual deben ponerse al final los dos puntos (:), el nombre de la clase padre
+y los atributos inicializados en el constructor de dicha clase. En las líneas 29 y 30 se indica que el cargo y el salario (atributos propios de la clase hija
+Empleado) deben ser iguales a las nuevas variables que serán ingresadas cuando se cree el objeto.*/
 Empleado::Empleado(string elNombre,int laEdad,string elCargo,float elSalario) : Persona(elNombre,laEdad){
   cargo = elCargo;
   salario = elSalario;
 }
-
+/*Creación de métodos clases "Empleado y Persona"
+Entre las líneas 33 y 37 se crea el método verEmpleado(), cuyo objetivo consiste en mostrar toda la información del empleado y teniendo en cuenta que
+Empleado  es la clase hija, hereda el método verPersona() de la clase padre; por lo tanto, la primera instrucción de dicho método consiste en invocar
+el método verPersona, lo cual mostrará el nombre y la edad, de modo que solo resta incluir las instrucciones que muestren el cargo y el salario, lo
+cual se realiza en las líneas 35 y 36. Entre las líneas 39 y 41 se crea el método verPersona() que se encarga de mostrar el nombre y la edad. */
 void Empleado::verEmpleado(){
   verPersona();
   cout<<"Cargo: "<<cargo<<endl;
@@ -39,7 +62,9 @@ void Persona::verPersona(){
   cout<<"Nombre: "<<nombre<<endl;
   cout<<"Edad: "<<edad<<endl;
 }
-
+/*Creación de objeto como instancia de la clase "Empleado"
+En la línea 45 se crea el objeto llamado empleado1 de la clase Empleado inicializando tanto los atributos heredados como los propios con los valores
+nombre Camila Soto, edad 19, cargo Recepcionista y salario 890000. En la línea 47 se indica al objeto empleado1 que ejecute el método verEmpleado().*/
 int main(){
   Empleado empleado1 = Empleado("Camila Soto",19,"Recepcionista",890000);
   empleado1.verEmpleado();
